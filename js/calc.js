7591d4c25970a9b0bcb7485181a38a23f23f7f0a
mahnoorbaig <mahnoorbaig.contact@gmail.com>
2018-08-13 16:58:19 +0000

    Done.

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,53 @@
 //add your code here
+$(document).ready(function(){
+  doMath();
+});
+    
+
+var num1 = $('#number1');
+var num2 = $('#number2');
+var operator = $('#operation');
+
+ function result(num1, num2, operator) {
+    var total = 0;
+    
+    if (operator === '+' )
+    {
+      total = num1 + num2;
+    }
+    
+    else if (operator === '-' )
+    {
+      total = num1 - num2;
+    }
+    
+    else if (operator === '*' )
+    {
+      total = num1 * num2;
+    }
+    else if (operator === '/' )
+    {
+      total = num1 / num2;
+    }
+
+    return total
+    
+
+    }
+ // console.log(total)
+  }
+
+function doMath(){
+  $("#equals").on('click', function(){
+  num1new = parseFloat(num1.val());
+  num2new = parseFloat(num2.val());
+  operatornew = operator.val();
+  
+  let ans = result(num1new, num2new, operatornew)
+  console.log(ans)
+
+   $("#result").html(ans)
+  
+  });
+ 
+}
