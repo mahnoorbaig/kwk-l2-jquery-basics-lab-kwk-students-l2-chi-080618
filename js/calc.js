d223b00a42b0d56324a053d921ae4f5481194130
mahnoorbaig <mahnoorbaig.contact@gmail.com>
2018-08-13 16:14:42 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -8,13 +8,7 @@ var num1 = $('#number1');
 var num2 = $('#number2');
 var operator = $('#operation');
 
-
-  function result(num1, num2, operator) {
-    
-    num1 = parseFloat(num1.val());
-    num2 = parseFloat(num2.val());
-    operator = operator.val();
-  
+ function result(num1, num2, operator) {
     if (operator === '+' )
     {
       var total = num1 + num2;
@@ -24,14 +18,18 @@ var operator = $('#operation');
 
   }
   console.log(total)
-  
-  
-  
+
+
+
 function doMath(){
   $("#equals").on('click', function(){
+  num1 = parseFloat(num1.val());
+  num2 = parseFloat(num2.val());
+  operator = operator.val();
   
-
-
+  
+  
+ 
 
   });
 }
